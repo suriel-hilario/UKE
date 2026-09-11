1c052772f1e47356d60bd529cd9317df4ae41c35
Suriel <suriel.hilario@servexternos.repsol.com>
2026-09-11 08:19:59 +0200

    fix ci/cd

--- deploy.sh
+++ deploy.sh
@@ -10,6 +10,9 @@ docker compose -f docker-compose.prod.yml --env-file .env.prod build
 echo "Applying database migrations..."
 docker compose -f docker-compose.prod.yml --env-file .env.prod run --rm api npx prisma migrate deploy
 
+docker compose -f docker-compose.prod.yml --env-file .env.prod exec -T api npx prisma generate
+docker compose -f docker-compose.prod.yml --env-file .env.prod exec -T api npx prisma db seed
+
 echo "Starting services..."
 docker compose -f docker-compose.prod.yml --env-file .env.prod up -d
 
